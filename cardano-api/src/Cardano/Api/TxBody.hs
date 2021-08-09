1208c9d18aa8bd272a88055a80b8dd8ee8f64d8f
Yuriy Syrovetskiy <yuriy.syrovetskiy@iohk.io>
2021-08-09 17:46:03 +0300

    cardano-api: Add Eq, Show instances needed for roundtrip tests

--- cardano-api/src/Cardano/Api/TxBody.hs
+++ cardano-api/src/Cardano/Api/TxBody.hs
@@ -156,6 +156,7 @@ import qualified Data.Set as Set
 import           Data.String (IsString)
 import           Data.Text (Text)
 import qualified Data.Text as Text
+import           Data.Type.Equality (TestEquality (..), (:~:) (Refl))
 import           Data.Word (Word32, Word64)
 import           GHC.Generics
 
@@ -425,6 +426,12 @@ data TxOutInAnyEra where
                    -> TxOutInAnyEra
 
 deriving instance Show TxOutInAnyEra
+
+instance Eq TxOutInAnyEra where
+  TxOutInAnyEra era1 out1 == TxOutInAnyEra era2 out2 =
+    case testEquality era1 era2 of
+      Just Refl -> out1 == out2
+      Nothing   -> False
 
 -- | Convenience constructor for 'TxOutInAnyEra'
 txOutInAnyEra :: IsCardanoEra era => TxOut era -> TxOutInAnyEra
@@ -1190,6 +1197,7 @@ data TxBodyContent build era =
        txMintValue      :: TxMintValue    build era,
        txScriptValidity :: BuildTxWith build (TxScriptValidity era)
      }
+     deriving (Eq, Show)
 
 
 -- ----------------------------------------------------------------------------
@@ -1556,7 +1564,7 @@ data TxBodyError =
      | TxBodyMintBeforeMaryError
      | TxBodyMissingProtocolParams
      | TxBodyInIxOverflow TxIn
-     deriving Show
+     deriving (Eq, Show)
 
 instance Error TxBodyError where
     displayError TxBodyEmptyTxIns  = "Transaction body has no inputs"
